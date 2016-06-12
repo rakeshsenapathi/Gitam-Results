bed3dd03a0846ffd2c13d7136c5df542b176ba2a
rakeshsenapathi <rakeshsenapathi@gmail.com>
2016-06-12 16:15:33 +0530

    Loaded Webview and backbtn enabled

---
+++ app/src/main/java/com/senapathi/gitamresults/BaseActivity.java
@@ -0,0 +1,26 @@
+package com.senapathi.gitamresults;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Senapathi on 12-06-2016.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        ButterKnife.bind(this);
+    }
+
+    protected abstract int getLayoutId();
+
+    public void enableBackButton() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
--- app/src/main/java/com/senapathi/gitamresults/MainActivity.java
+++ app/src/main/java/com/senapathi/gitamresults/MainActivity.java
@@ -1,13 +1,55 @@
 package com.senapathi.gitamresults;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+
+public class MainActivity extends BaseActivity {
+
+    @Bind(R.id.webView)
+    protected WebView webView;
+    private String homeURL = "https://eweb.gitam.edu/mobile/Pages/NewGrdcrdInput1.aspx";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Gitam Semester Results");
+        // Loading Webview
+        webView.loadUrl(homeURL);
+        webView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // do your handling codes here, which url is the requested url
+                // probably you need to open that url rather than redirect:
+                view.loadUrl(url);
+                return false; // then it is not handled by default action
+            }
+        });
+        //
     }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 }
